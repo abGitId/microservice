ed8c604f0a9a14af2ca86aa039e27c4df33ccb6d
abGitId <abhi.kadam397@gmail.com>
2021-10-19 18:50:46 +0530

    Add files via upload
    eureka server added

---
+++ esurekaserver/src/test/java/com/example/esurekaserver/EsurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
